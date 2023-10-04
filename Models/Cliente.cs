a0d1759f8081474440f9bbdd6e05105344b209f6
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-10-04 10:10:23 -0300

    Cliente corregido

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -3,19 +3,19 @@ namespace EspacioCliente
     public class Cliente
     {
         // Atributos
-        string? nombre;
-        string? direccion;
-        int telefono;
-        string? datosReferenciaDireccion;
+        string nombre;
+        string direccion;
+        string telefono;
+        string datosReferenciaDireccion;
 
         //Propiedades
-        public string? Nombre { get => nombre;  }
-        public string? Direccion { get => direccion; }
-        public int Telefono { get => telefono;  }
-        public string? DatosReferenciaDireccion { get => datosReferenciaDireccion;  }
+        public string Nombre { get => nombre;  }
+        public string Direccion { get => direccion; }
+        public string Telefono { get => telefono;  }
+        public string DatosReferenciaDireccion { get => datosReferenciaDireccion;  }
         
         // Constructores
-        public Cliente(string nombre, string direccion, int telefono, string datosReferenciaDireccion)
+        public Cliente(string nombre, string direccion, string telefono, string datosReferenciaDireccion)
         {
             this.nombre = nombre;
             this.direccion = direccion;
